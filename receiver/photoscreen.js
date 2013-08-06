74c0af8897193206217c1939ea0d4ab72b004a25
Motohiro Takayama <mootoh@gmail.com>
2013-08-06 10:59:44 -0700

    Using Flickr for prototype.

--- receiver/photoscreen.js
+++ receiver/photoscreen.js
@@ -4,16 +4,17 @@ var cast = window.cast || {};
 (function() {
   'use strict';
 
-  Photoscreen.PROTOCOL = "net.mootoh.cast.photoscreen";
+  PhotoScreen.PROTOCOL = "net.mootoh.cast.photoscreen";
 
-  function Photoscreen() {
+  function PhotoScreen(img) {
+    this.img = img;
     this.channelHandler_ = new cast.receiver.ChannelHandler('PhotoScreenDebug');
     this.channelHandler_.addEventListener(cast.receiver.Channel.EventType.MESSAGE, this.onMessage.bind(this));
     this.channelHandler_.addEventListener(cast.receiver.Channel.EventType.OPEN, this.onChannelOpened.bind(this));
     this.channelHandler_.addEventListener(cast.receiver.Channel.EventType.CLOSED, this.onChannelClosed.bind(this));
   }
 
-  Photoscreen.prototype = {
+  PhotoScreen.prototype = {
     onChannelOpened: function(event) {
       console.log('onChannelOpened. Total number of channels: ' + this.channelHandler_.getChannels().length);
     },
@@ -28,10 +29,20 @@ var cast = window.cast || {};
 
     onMessage: function(event) {
       var message = event.message;
-      var channel = event.target;
+      var idx = message.image_url;
 
       console.log('********onMessage********' + JSON.stringify(message));
+
+      var image_urls = [
+        // "http://farm8.staticflickr.com/2836/9342179435_ea1361f989.jpg",
+        "http://farm3.staticflickr.com/2887/9188577195_2945b5443d.jpg",
+        "http://farm6.staticflickr.com/5471/9342194129_8b7ce4c67f.jpg",
+        "http://farm3.staticflickr.com/2836/9342193489_e72a63ce4a.jpg"];
+
+      console.log("image url = " + image_urls[idx-1]);
+      this.img.src = image_urls[idx-1];
+    }
   };
 
-  cast.Photoscreen = Photoscreen;
+  cast.PhotoScreen = PhotoScreen;
 })();
